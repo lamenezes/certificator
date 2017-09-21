cbffb0f17a3a373179fc99bfe94c42f56174aaf5
Luiz Menezes <luiz.menezesf@gmail.com>
2017-09-21 09:30:55 -0300

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='certificator',
-    version='0.0.1',
+    version='0.0.2',
     description='Certificate Generator Tool and Library',
     url='https://github.com/lamenezes/certificator',
     author='Luiz Menezes',
