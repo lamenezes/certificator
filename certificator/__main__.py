5b863c76f65b5e26e412ad5e1bb60abc76680076
jecs89 <jecs89@gmail.com>
2017-10-24 20:26:31 -0200

    Fix event-id argument name

--- certificator/__main__.py
+++ certificator/__main__.py
@@ -6,7 +6,7 @@ from .meetup import MeetupCertificator
 @click.command()
 @click.argument('destination', type=click.Path(exists=True, dir_okay=True), default='.')
 @click.option('--urlname', '-u')
-@click.option('--event_id', '-e')
+@click.option('--event-id', '-e')
 @click.option('--meetup-api-key', '-k')
 @click.option('--template-path', '-t', type=click.Path(exists=True, dir_okay=True), default=None)
 def generate(destination, urlname, event_id, meetup_api_key, template_path):
